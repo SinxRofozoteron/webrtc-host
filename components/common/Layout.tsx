8752b4b4e367b5d41142a8d0e061b2eb741411b2
unknown <alex.burakou@gmail.com>
2023-03-04 15:57:15 -0700

    Setup

---
+++ components/common/Layout.tsx
@@ -0,0 +1,20 @@
+import { ReactNode } from 'react';
+import Head from 'next/head';
+
+import { Header } from './header/Header';
+
+type LayoutProps = {
+  children: ReactNode;
+};
+
+export const Layout = ({ children }: LayoutProps) => {
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <Header />
+      {children}
+    </>
+  );
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+
+import { Layout } from '../components/common/Layout';
+import { ThemeProvider } from '../components/wrappers/themeProvider/ThemeProvider';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ThemeProvider>
+  );
 }
