1e6bf851f3883cf1b010b2bbf9abb5499cdb471a
unknown <alex.burakou@gmail.com>
2023-03-16 07:52:00 -0600

    Added session provider to _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,24 @@
 import '../styles/globals.css';
 
+import { SessionProvider } from 'next-auth/react';
+
 import { Layout } from '../src/components/common/Layout';
 import { ThemeProvider, IntlProvider } from '../src/components/wrappers';
 
 import type { AppProps } from 'next/app';
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps }
+}: AppProps) {
   return (
     <IntlProvider locale="en">
       <ThemeProvider>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
+        <SessionProvider session={session}>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </SessionProvider>
       </ThemeProvider>
     </IntlProvider>
   );
