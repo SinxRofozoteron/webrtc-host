2755e1cca207e54ac4931dd954cd52787adb567d
unknown <alex.burakou@gmail.com>
2023-03-16 07:10:53 -0600

    Added nextauth api route

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,23 +1,58 @@
-import NextAuth from 'next-auth';
+import NextAuth, { type NextAuthOptions } from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
+import { type NextApiRequest, type NextApiResponse } from 'next';
 
-import { axiosBackend } from '../../../services/axiosBackend';
+import { axiosBackend } from '../../../src/services/axiosBackend';
+import { isEmail } from '../../../src/utils';
+import { type LoginCredentials } from '../../../src/types';
 
-const authOptions = {
-  providers: [
-    CredentialsProvider({
-      name: 'Credentials',
-      credentials: {
-        username: { label: 'Username or password', type: 'text' },
-        password: { label: 'Password', type: 'password' }
-      },
-      async authorize(credentials) {
-        try {
-          const response = axiosBackend.post('login', {});
-        } catch (err) {}
-      }
-    })
-  ]
+const authOptionsCallback = (
+  req: NextApiRequest,
+  res: NextApiResponse
+): NextAuthOptions => {
+  return {
+    providers: [
+      CredentialsProvider({
+        name: 'Credentials',
+        credentials: {
+          login: { label: 'Username or password', type: 'text' },
+          password: { label: 'Password', type: 'password' }
+        },
+        async authorize(credentials) {
+          if (!credentials) {
+            return null;
+          }
+          let creds: LoginCredentials = {
+            username: credentials.login,
+            password: credentials.password
+          };
+          // If login is email-like supply email as well
+          // email-like login could potentially be a username as well
+          // that's why we supply both email and username
+          if (isEmail(credentials.login)) {
+            creds.email = credentials.login;
+          }
+
+          try {
+            const response = await axiosBackend.post('login', creds);
+
+            const cookies = response.headers['set-cookie']!;
+            res.setHeader('Set-Cookie', cookies);
+
+            return response.data;
+          } catch (err) {
+            // TODO: Remove console.log
+            console.error(err);
+            return null;
+          }
+        }
+      })
+    ]
+  };
 };
 
-export default NextAuth(authOptions);
+const auth = (req: NextApiRequest, res: NextApiResponse) => {
+  return NextAuth(req, res, authOptionsCallback(req, res));
+};
+
+export default auth;
