b796e7548eab438babf0e5de03d6fa48b605f1b1
unknown <alex.burakou@gmail.com>
2023-04-03 16:35:05 -0600

    Modified VideoConfig to use context + modified tests

--- src/components/video/VideoConfig.test.tsx
+++ src/components/video/VideoConfig.test.tsx
@@ -1,18 +1,16 @@
 import { render, screen, act } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
-import { MOCK_MEDIA_DEVICES } from '../../../testUtils';
+import { MOCK_MEDIA_DEVICES, DeviceMock } from '../../../testUtils';
 import { DEVICE_KINDS } from '../../constants';
 import { AppWrapper } from '../wrappers';
+import messages from '../../locales';
 
 import { VideoConfig } from './VideoConfig';
+import { MediaDeviceContext } from './MediaDeviceContext';
 
 describe('<VideoConfig />', () => {
   const user = userEvent.setup();
-
-  const enumerateDevicesMock = jest.fn().mockResolvedValue(MOCK_MEDIA_DEVICES);
-  const addEventListenerMock = jest.fn();
-  const removeEventListenerMock = jest.fn();
 
   let valueMock: string = MOCK_MEDIA_DEVICES.find(
     device => device.kind === DEVICE_KINDS.VIDEO
@@ -21,91 +19,131 @@ describe('<VideoConfig />', () => {
     valueMock = deviceId;
   });
 
-  const videoDevices = MOCK_MEDIA_DEVICES.filter(
-    device => device.kind === DEVICE_KINDS.VIDEO
-  );
+  describe('with full media device info', () => {
+    const videoDevices = MOCK_MEDIA_DEVICES.filter(
+      device => device.kind === DEVICE_KINDS.VIDEO
+    );
 
-  beforeAll(() => {
-    Object.defineProperty(navigator, 'mediaDevices', {
-      value: {
-        enumerateDevices: enumerateDevicesMock,
-        addEventListener: addEventListenerMock,
-        removeEventListener: removeEventListenerMock
+    beforeEach(async () => {
+      await act(async () =>
+        render(
+          <AppWrapper>
+            <MediaDeviceContext.Provider value={MOCK_MEDIA_DEVICES}>
+              <VideoConfig onChange={onChangeMock} value={valueMock} />
+            </MediaDeviceContext.Provider>
+          </AppWrapper>
+        )
+      );
+    });
+
+    afterEach(() => {
+      jest.clearAllMocks();
+
+      // Reset value mock
+      valueMock = MOCK_MEDIA_DEVICES.find(
+        device => device.kind === DEVICE_KINDS.VIDEO
+      )!.deviceId;
+    });
+
+    it('number of rendered radio buttons matches number of video devices', async () => {
+      const expectedNumOfRadioBtns = videoDevices.length;
+
+      const radioBtns = screen.getAllByRole('radio');
+
+      expect(radioBtns).toHaveLength(expectedNumOfRadioBtns);
+    });
+
+    it.each(videoDevices.map(device => device.label))(
+      'renders "%s" device radio button',
+      async label => {
+        const radio = screen.queryByRole('radio', { name: label });
+        expect(radio).toBeInTheDocument();
       }
+    );
+
+    it('renders radio with provided value as checked', async () => {
+      const name = MOCK_MEDIA_DEVICES.find(
+        device => device.deviceId === valueMock
+      )!.label;
+
+      const radio = screen.getByRole('radio', { name });
+      expect(radio).toBeChecked();
+    });
+
+    it('calls onChange with correct deviceId when new device is selected', async () => {
+      const { label: newDeviceName, deviceId: newDeviceId } = MOCK_MEDIA_DEVICES.find(
+        // Find second video device which is not currently selected
+        device => device.kind === DEVICE_KINDS.VIDEO && device.deviceId !== valueMock
+      )!;
+
+      const radio = screen.getByRole('radio', { name: newDeviceName });
+      await user.click(radio);
+
+      expect(onChangeMock).toHaveBeenCalledTimes(1);
+      expect(onChangeMock).toHaveBeenLastCalledWith(newDeviceId);
+    });
+
+    it('radio group has accessible name', async () => {
+      const group = screen.getByRole('radiogroup');
+      expect(group).toHaveAccessibleName();
     });
   });
 
-  beforeEach(async () => {
-    await act(async () =>
-      render(
-        <AppWrapper>
-          <VideoConfig onChange={onChangeMock} value={valueMock} />
-        </AppWrapper>
-      )
+  describe('with media devices without labels', () => {
+    // Create new devices without labels
+    const mock_media_devices = MOCK_MEDIA_DEVICES.map(
+      device => new DeviceMock(device.deviceId, device.kind, '', device.groupId)
     );
+
+    beforeEach(async () => {
+      await act(async () =>
+        render(
+          <AppWrapper>
+            <MediaDeviceContext.Provider value={mock_media_devices}>
+              <VideoConfig onChange={onChangeMock} value={valueMock} />
+            </MediaDeviceContext.Provider>
+          </AppWrapper>
+        )
+      );
+    });
+
+    it('does not render radio group', () => {
+      const radioGroup = screen.queryByRole('radiogroup');
+      expect(radioGroup).not.toBeInTheDocument();
+    });
+
+    it('renders waiting text', () => {
+      const waitingText = screen.queryByText(
+        messages.en['callWindow.waitingForPermissionsText']
+      );
+      expect(waitingText).toBeVisible();
+    });
   });
 
-  afterEach(() => {
-    jest.clearAllMocks();
+  describe('with no media devices', () => {
+    beforeEach(async () => {
+      await act(async () =>
+        render(
+          <AppWrapper>
+            <MediaDeviceContext.Provider value={null}>
+              <VideoConfig onChange={jest.fn()} value={null} />
+            </MediaDeviceContext.Provider>
+          </AppWrapper>
+        )
+      );
+    });
 
-    // Reset value mock
-    valueMock = MOCK_MEDIA_DEVICES.find(
-      device => device.kind === DEVICE_KINDS.VIDEO
-    )!.deviceId;
-  });
+    it('does not render waiting text', () => {
+      const waitingText = screen.queryByText(
+        messages.en['callWindow.waitingForPermissionsText']
+      );
+      expect(waitingText).not.toBeInTheDocument();
+    });
 
-  it('calls enumerateDevices on initial render', () => {
-    expect(enumerateDevicesMock).toHaveBeenCalledTimes(1);
-  });
+    it('renders empty radio group', () => {
+      const radioGroup = screen.getByRole('radiogroup');
 
-  it('calls addEventListener on initial render with correct event', () => {
-    expect(addEventListenerMock).toHaveBeenCalledTimes(1);
-    expect(addEventListenerMock).toHaveBeenCalledWith(
-      'devicechange',
-      expect.anything()
-    );
-  });
-
-  it('number of rendered radio buttons matches number of video devices', async () => {
-    const expectedNumOfRadioBtns = videoDevices.length;
-
-    const radioBtns = screen.getAllByRole('radio');
-
-    expect(radioBtns).toHaveLength(expectedNumOfRadioBtns);
-  });
-
-  it.each(videoDevices.map(device => device.label))(
-    'renders "%s" device radio button',
-    async label => {
-      const radio = screen.queryByRole('radio', { name: label });
-      expect(radio).toBeInTheDocument();
-    }
-  );
-
-  it('renders radio with provided value as checked', async () => {
-    const name = MOCK_MEDIA_DEVICES.find(
-      device => device.deviceId === valueMock
-    )!.label;
-
-    const radio = screen.getByRole('radio', { name });
-    expect(radio).toBeChecked();
-  });
-
-  it('calls onChange with correct deviceId when new device is selected', async () => {
-    const { label: newDeviceName, deviceId: newDeviceId } = MOCK_MEDIA_DEVICES.find(
-      // Find second video device which is not currently selected
-      device => device.kind === DEVICE_KINDS.VIDEO && device.deviceId !== valueMock
-    )!;
-
-    const radio = screen.getByRole('radio', { name: newDeviceName });
-    await user.click(radio);
-
-    expect(onChangeMock).toHaveBeenCalledTimes(1);
-    expect(onChangeMock).toHaveBeenLastCalledWith(newDeviceId);
-  });
-
-  it('radio group has accessible name', async () => {
-    const group = screen.getByRole('radiogroup');
-    expect(group).toHaveAccessibleName();
+      expect(radioGroup.children).toHaveLength(0);
+    });
   });
 });
--- src/components/video/VideoConfig.tsx
+++ src/components/video/VideoConfig.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useContext, useMemo } from 'react';
 import FormControl from '@mui/material/FormControl';
 import FormLabel from '@mui/material/FormLabel';
 import { FormattedMessage } from 'react-intl';
@@ -8,6 +8,9 @@ import Radio from '@mui/material/Radio';
 import { styled } from '@mui/material/styles';
 
 import { DEVICE_KINDS } from '../../constants';
+
+import { MediaDeviceContext } from './MediaDeviceContext';
+import { NoPermissionText } from './NoPermissionText';
 
 const StyledFormLabel = styled(FormLabel)(({ theme }) => ({
   textAlign: 'center',
@@ -26,53 +29,51 @@ type VideoConfigProps = {
 };
 
 export const VideoConfig = ({ onChange, value }: VideoConfigProps) => {
-  const [devices, setDevices] = useState<MediaDeviceInfo[] | null>(null);
+  const devices = useContext(MediaDeviceContext);
 
-  const updateDevices = useCallback(() => {
-    if (!navigator.mediaDevices?.enumerateDevices) {
-      setDevices(null);
+  // If no video devices present set this variable to []
+  // If video devices don't have labels it means user did not grant permission.
+  // Set it to null
+  const videoDevices = useMemo<MediaDeviceInfo[] | null>(() => {
+    let devs: MediaDeviceInfo[] | null = [];
+
+    if (devices) {
+      for (const device of devices) {
+        if (device.kind === DEVICE_KINDS.VIDEO) {
+          if (!device.label) {
+            devs = null;
+            break;
+          }
+          devs.push(device);
+        }
+      }
     }
 
-    navigator.mediaDevices
-      .enumerateDevices()
-      .then(devs => {
-        const videoDevices = devs.filter(dev => dev.kind === DEVICE_KINDS.VIDEO);
-        setDevices(videoDevices);
-      })
-      .catch(err => console.log(err));
-  }, []);
-
-  // Get devices on initial render
-  useEffect(() => {
-    updateDevices();
-  }, [updateDevices]);
-
-  // Setup listener to update devices on devicechange event
-  useEffect(() => {
-    navigator.mediaDevices.addEventListener('devicechange', updateDevices);
-
-    return () =>
-      navigator.mediaDevices.removeEventListener('devicechange', updateDevices);
-  }, [updateDevices]);
+    return devs;
+  }, [devices]);
 
   return (
     <FormControl>
       <StyledFormLabel id={LABEL_ID}>
         <FormattedMessage id="callWindow.videoConfig.label" />
       </StyledFormLabel>
-      <RadioGroup
-        aria-labelledby={LABEL_ID}
-        onChange={e => onChange(e.target.value)}
-        value={value}>
-        {devices?.map(device => (
-          <StyledFromControlLabel
-            value={device.deviceId}
-            control={<Radio />}
-            label={device.label}
-            key={device.deviceId}
-          />
-        ))}
-      </RadioGroup>
+      {videoDevices ? (
+        <RadioGroup
+          aria-labelledby={LABEL_ID}
+          onChange={e => onChange(e.target.value)}
+          value={value}>
+          {videoDevices.map(device => (
+            <StyledFromControlLabel
+              value={device.deviceId}
+              control={<Radio />}
+              label={device.label}
+              key={device.deviceId}
+            />
+          ))}
+        </RadioGroup>
+      ) : (
+        <NoPermissionText />
+      )}
     </FormControl>
   );
 };
