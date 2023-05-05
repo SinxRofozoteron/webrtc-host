378c60593c02bf7a32a6c70c0579d3813a1ffa79
unknown <alex.burakou@gmail.com>
2023-05-05 10:11:41 -0600

    Added constants

---
+++ src/constants/media.ts
@@ -0,0 +1,5 @@
+export const DEVICE_KINDS = {
+  VIDEO: 'videoinput',
+  AUDIO_INPUT: 'audioinput',
+  AUDIO_OUTPUT: 'audiooutput'
+} as const;
---
+++ src/constants/styleConstants.ts
@@ -0,0 +1,4 @@
+export const Z_INDEX_LEVELS = {
+  ALWAYS_VISIBLE: 100,
+  BOTTOM: 0
+};
