ab32fd57fab58dd21448782f0881acbb5cc6ceb3
unknown <alex.burakou@gmail.com>
2023-06-27 11:28:20 -0600

    Page Container core

---
+++ src/components/core/layout/PageContainer.tsx
@@ -0,0 +1,20 @@
+import Box, { type BoxProps } from '@mui/material/Box';
+import { styled } from '@mui/material/styles';
+import get from 'lodash/get';
+
+type PageContainerProps = BoxProps & {
+  bgColor?: string;
+};
+
+export const PageContainer = styled(Box, {
+  shouldForwardProp: prop => prop !== 'bgColor'
+})<PageContainerProps>(({ theme, ...props }) => ({
+  height: '100vh',
+  width: '100vw',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  backgroundColor: props.bgColor
+    ? get(theme.palette, props.bgColor)
+    : theme.palette.background.default
+}));
