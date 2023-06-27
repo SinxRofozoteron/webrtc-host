8b96c3d926340ce729a2d0ea81c4ecec09306d1b
unknown <alex.burakou@gmail.com>
2023-06-27 11:27:18 -0600

    Core Button

--- src/components/core/Button.tsx
+++ src/components/core/Button.tsx
@@ -1,12 +1,33 @@
-import ButtonMui from '@mui/material/Button';
+import ButtonMui, { type ButtonProps as ButtonPropsMui } from '@mui/material/Button';
 import { styled } from '@mui/material/styles';
 
-export const Button = styled(ButtonMui)(({ theme }) => ({
+type ButtonProps = ButtonPropsMui & {
+  maxWidth?: string;
+};
+
+export const Button = styled(ButtonMui, {
+  shouldForwardProp: prop => prop !== 'maxWidth'
+})<ButtonProps>(({ theme, ...props }) => ({
+  gap: theme.spacing(0.5),
   width: '100%',
-  color: theme.palette.text.secondary,
+  maxWidth: props.maxWidth,
+  color: theme.palette.text.primary,
   margin: theme.spacing(1),
-  backgroundColor: theme.palette.secondary.main,
+  backgroundColor: theme.palette.primary.main,
   '&:hover': {
-    backgroundColor: theme.palette.secondary.light
+    backgroundColor: theme.palette.primary.light,
+    color: theme.palette.text.secondary,
+    borderColor: theme.palette.text.secondary
+  },
+  borderColor: theme.palette.text.primary,
+  [theme.breakpoints.up('sm')]: {
+    gap: theme.spacing(2)
+  },
+  [theme.breakpoints.up('md')]: {
+    gap: theme.spacing(3)
+  },
+  '&.Mui-disabled': {
+    backgroundColor: theme.palette.primary.disabled,
+    color: theme.palette.text.disabled
   }
 }));
