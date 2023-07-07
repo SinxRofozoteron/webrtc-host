ddf352c06f2f245d29974f51f2393a6f6535f461
unknown <alex.burakou@gmail.com>
2023-07-07 06:47:46 -0600

    Configured shared modules

--- next.config.js
+++ next.config.js
@@ -1,19 +1,22 @@
 const { ModuleFederationPlugin } = require('webpack').container;
 
-const federationConfig = {
-  name: 'host',
-  filename: 'remoteEntry.js',
-  remotes: {
-    webrtcMfe: 'webrtcMfe@http://localhost:3002/remoteEntry.js'
-  }
-};
+const { dependencies } = require('./package.json');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   webpack: (config, options) => {
     if (!options.isServer) {
-      config.plugins.push(new ModuleFederationPlugin(federationConfig));
+      config.plugins.push(
+        new ModuleFederationPlugin({
+          name: 'host',
+          filename: 'remoteEntry.js',
+          remotes: {
+            webrtcMfe: 'webrtcMfe@http://localhost:3002/remoteEntry.js'
+          },
+          shared: dependencies
+        })
+      );
     }
 
     return config;
