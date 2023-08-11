60d638b2b73be21742f9f590922777098120000d
unknown <alex.burakou@gmail.com>
2023-08-11 07:27:55 -0600

    Fixed tests in NewUserFormInput

--- src/components/newUserPage/NewUserFormInput.test.tsx
+++ src/components/newUserPage/NewUserFormInput.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
-import { AppWrapper } from '../../wrappers';
+import { AppWrapper } from '../shared';
 
 import { NewUserFormInput } from './NewUserFormInput';
 
